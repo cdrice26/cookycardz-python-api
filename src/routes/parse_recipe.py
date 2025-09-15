16b81c19a561d919a1c410546560696060fd65c9
cdrice26 <156816748+cdrice26@users.noreply.github.com>
2025-09-15 18:26:33 -0400

    Switched recipe parser to POST

--- src/routes/parse_recipe.py
+++ src/routes/parse_recipe.py
@@ -1,18 +1,21 @@
-from fastapi import APIRouter, Query
+from fastapi import APIRouter
 from models.recipe import Recipe
 from services.recipe_parser import RecipeParser
 from urllib.parse import unquote
-from typing import Annotated
+from pydantic import BaseModel
 from fastapi_simple_rate_limiter import rate_limiter  # type: ignore
 
 router = APIRouter()
 
 
-@router.get("/parse-recipe")
+class ParseRecipeRequest(BaseModel):
+    url: str
+    html: str
+
+
+@router.post("/parse-recipe")
 @rate_limiter(limit=5, seconds=60)
-async def parse_recipe(
-    url: Annotated[str, Query()], html: Annotated[str, Query()]
-) -> Recipe:
-    parser = RecipeParser(unquote(url), unquote(html))
+async def parse_recipe(body: ParseRecipeRequest) -> Recipe:
+    parser = RecipeParser(unquote(body.url), unquote(body.html))
     parser.parse()
     return parser.get_as_recipe()
